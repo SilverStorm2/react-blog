fe8f9b20515445b349b17db362cb3a9d2ec41c50
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2025-11-11 01:39:10 +0100

    feat: add layout components and refactor Post route
    - Added layout components:
      - NavBar (renders static text),
      - Header (wraps NavBar),
      - Footer (outputs 'Footer').
    
    - Replaced Post with SinglePost in src/components/pages/SinglePost.js to match /post/:id route.
    - Updated App.js to import Container, Header, Footer, and SinglePost.
    - Routed layout inside <Container>, placing Header above and Footer below routes.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Home from './components/pages/Home';
-import Post from './components/pages/Post';
+import SinglePost from './components/pages/SinglePost';
 import PostAdd from './components/pages/PostAdd';
 import PostEdit from './components/pages/PostEdit';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
+import Header from './components/views/Header';
+import Footer from './components/views/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:id" element={<Post />} />
-      <Route path="/post/add" element={<PostAdd />} />
-      <Route path="/post/edit/:id" element={<PostEdit />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<SinglePost />} />
+        <Route path="/post/add" element={<PostAdd />} />
+        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
